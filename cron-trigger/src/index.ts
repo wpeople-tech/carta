b97d98ed6eb2d3fae3f8ae5efbd13e8dd01cb379
zidaneNS <zidane.nur004@gmail.com>
2026-07-12 06:38:34 +0700

    dev cron

---
+++ cron-trigger/src/index.ts
@@ -0,0 +1,56 @@
+/**
+ * Welcome to Cloudflare Workers!
+ *
+ * This is a template for a Scheduled Worker: a Worker that can run on a
+ * configurable interval:
+ * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your Worker in action
+ * - Run `npm run deploy` to publish your Worker
+ *
+ * Bind resources to your Worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(req) {
+		const url = new URL(req.url);
+		url.pathname = '/__scheduled';
+		url.searchParams.append('cron', '*/5 0-1 * * *');
+		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
+	},
+
+	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
+	// [[triggers]] configuration.
+	async scheduled(event, env, ctx): Promise<void> {
+		// A Cron Trigger can make requests to other endpoints on the Internet,
+		// publish to a Queue, query a D1 Database, and much more.
+		//
+		// We'll keep it simple and make an API call to a Cloudflare API:
+		try {
+			let resp1 = await fetch('https://cartatrade.tech/api/cron/refresh-analysis', {
+				headers: {
+					'authorization': `Bearer ${env.CRON_SECRET}`,
+				}
+			});
+			let resp2 = await fetch('https://cartatrade.tech/api/cron/refresh-prices', {
+				headers: {
+					'authorization': `Bearer ${env.CRON_SECRET}`,
+				}
+			});
+			console.log('resp1', resp1.headers)
+			let wasSuccessful1 = resp1.ok ? 'success' : 'fail';
+			let wasSuccessful2 = resp2.ok ? 'success' : 'fail';
+
+			// You could store this result in KV, write to a D1 Database, or publish to a Queue.
+			// In this template, we'll just log the result:
+			console.log(`trigger fired at ${event.cron}: ${wasSuccessful1}`);
+			console.log(`trigger fired at ${event.cron}: ${wasSuccessful2}`);
+		} catch (err) {
+			console.error('cron:::ERROR:', err);
+		}
+	},
+} satisfies ExportedHandler<Env>;
